2a6d7723c0b257715e9f577ce1443def8ae4c466
Doff <drdoffensmirhtz@gmail.com>
2026-01-15 10:13:28 +0000

    Clase Movie generada

---
+++ src/main/java/software/ulpgc/kata2/application/Main.java
@@ -0,0 +1,7 @@
+package software.ulpgc.kata2.application;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/software/ulpgc/kata2/model/Movie.java
@@ -0,0 +1,4 @@
+package software.ulpgc.kata2.model;
+
+public record Movie(String title, int releaseYear, int duration) {
+}
